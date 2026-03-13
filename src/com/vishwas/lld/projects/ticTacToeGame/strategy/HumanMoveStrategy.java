b5bc461842bd915c79191fefa50b14e6ec91478f
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-13 19:10:13 +0530

    Update HumanMoveStrategy.java

--- src/com/vishwas/lld/projects/ticTacToeGame/strategy/HumanMoveStrategy.java
+++ src/com/vishwas/lld/projects/ticTacToeGame/strategy/HumanMoveStrategy.java
@@ -1,4 +1,18 @@
 package com.vishwas.lld.projects.ticTacToeGame.strategy;
 
-public class HumanMoveStrategy {
+import com.vishwas.lld.projects.ticTacToeGame.model.Board;
+import com.vishwas.lld.projects.ticTacToeGame.model.Symbol;
+
+import java.util.Scanner;
+
+public class HumanMoveStrategy implements MoveStrategy{
+    private final Scanner sc = new Scanner(System.in);
+
+    @Override
+    public int[] determineMove(Board board, Symbol symbol) {
+        System.out.print("Player "+ symbol+" enter row and col (0 0 to 2 2: ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        return new int[]{r, c};
+    }
 }
