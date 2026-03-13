9d8bd3ea4b634d5881213800e20e0e019b33a367
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-13 20:05:54 +0530

    Update Main.java

--- src/com/vishwas/lld/projects/ticTacToeGame/Main.java
+++ src/com/vishwas/lld/projects/ticTacToeGame/Main.java
@@ -5,12 +5,18 @@ import com.vishwas.lld.projects.ticTacToeGame.model.Player;
 import com.vishwas.lld.projects.ticTacToeGame.model.Symbol;
 import com.vishwas.lld.projects.ticTacToeGame.strategy.HumanMoveStrategy;
 import com.vishwas.lld.projects.ticTacToeGame.strategy.MoveStrategy;
+import com.vishwas.lld.projects.ticTacToeGame.strategy.RandomBotStrategy;
 
 public class Main {
     public static void main(String[] args) {
-        MoveStrategy strategy = new HumanMoveStrategy();
-        Player p1 = new Player(strategy, Symbol.X);
-        Player p2 = new Player(strategy, Symbol.O);
+        // Player 1 is a Human
+        Player p1 = new Player(new HumanMoveStrategy(), Symbol.X);
+
+//        // Player 2 is a Human
+//        Player p2 = new Player(new HumanMoveStrategy(), Symbol.O);
+
+        // Player 2 is AI Bot
+        Player p2 = new Player(new RandomBotStrategy(), Symbol.O);
 
         TicTacToeGame game = new TicTacToeGame(p1, p2, 3);
         game.start();
