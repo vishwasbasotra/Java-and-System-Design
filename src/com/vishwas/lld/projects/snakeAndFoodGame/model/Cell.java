aef10207e2a72dac007ac6ad9c996ad7d10b3f0e
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-18 17:10:48 +0530

    Update Cell.java

--- src/com/vishwas/lld/projects/snakeAndFoodGame/model/Cell.java
+++ src/com/vishwas/lld/projects/snakeAndFoodGame/model/Cell.java
@@ -3,6 +3,28 @@ package com.vishwas.lld.projects.snakeAndFoodGame.model;
 import com.vishwas.lld.projects.snakeAndFoodGame.enums.CellType;
 
 public class Cell {
-    private int x,y;
+    private int row, col;
     private CellType type;
+
+    public Cell(int row, int col, CellType type) {
+        this.row = row;
+        this.col = col;
+        this.type = CellType.EMPTY;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public CellType getType() {
+        return type;
+    }
+
+    public void setType(CellType type) {
+        this.type = type;
+    }
 }
