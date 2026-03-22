d24d588540df3f1c69464d59ae90398574d91bdc
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-22 08:17:58 +0530

    Update ParkingSlot.java

--- src/com/vishwas/lld/projects/parkingLot/model/ParkingSlot.java
+++ src/com/vishwas/lld/projects/parkingLot/model/ParkingSlot.java
@@ -3,15 +3,28 @@ package com.vishwas.lld.projects.parkingLot.model;
 import com.vishwas.lld.projects.parkingLot.enums.ParkingSlotType;
 import com.vishwas.lld.projects.parkingLot.enums.ParkingSpaceStatus;
 
+import java.util.Map;
+
 public abstract class ParkingSlot {
-    private static long parkingSlotId = 0;
+    private String parkingSlotId;
     private ParkingSpaceStatus parkingSpaceStatus;
     private ParkingSlotType parkingSlotType;
-
     public ParkingSlot(ParkingSlotType parkingSlotType) {
         parkingSlotId += 1;
         this.parkingSpaceStatus = ParkingSpaceStatus.AVAILABLE;
         this.parkingSlotType = parkingSlotType;
     }
     public abstract boolean canFit(Vehicle vehicle);
+
+    public ParkingSpaceStatus getParkingSpaceStatus() {
+        return parkingSpaceStatus;
+    }
+
+    public void setParkingSpaceStatus(ParkingSpaceStatus parkingSpaceStatus) {
+        this.parkingSpaceStatus = parkingSpaceStatus;
+    }
+
+    public ParkingSlotType getParkingSlotType() {
+        return parkingSlotType;
+    }
 }
