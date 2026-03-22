d127d0de418e5be22cbce5e8141dc910f360a48b
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-22 07:30:53 +0530

    Create LargeSlot.java

---
+++ src/com/vishwas/lld/projects/parkingLot/model/LargeSlot.java
@@ -0,0 +1,16 @@
+package com.vishwas.lld.projects.parkingLot.model;
+
+import com.vishwas.lld.projects.parkingLot.enums.ParkingSlotType;
+import com.vishwas.lld.projects.parkingLot.enums.VehicleType;
+
+public class LargeSlot extends ParkingSlot{
+    public LargeSlot(String parkingSlotId, int row, int col) {
+        super(parkingSlotId, ParkingSlotType.LARGE, row, col);
+    }
+
+    @Override
+    public boolean canFit(Vehicle vehicle){
+        return vehicle.getVehicleType() == VehicleType.TUCK ||
+                vehicle.getVehicleType() == VehicleType.CAR;
+    }
+}
