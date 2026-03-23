af3802845f9482dcaa20353574d77d532e85a0c1
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-23 06:41:05 +0530

    Update Vehicle.java

--- src/com/vishwas/lld/projects/parkingLot/model/Vehicle.java
+++ src/com/vishwas/lld/projects/parkingLot/model/Vehicle.java
@@ -2,8 +2,19 @@ package com.vishwas.lld.projects.parkingLot.model;
 
 import com.vishwas.lld.projects.parkingLot.enums.VehicleType;
 
-public class Vehicle {
+public abstract class Vehicle {
+    private String vehicleNumber;
     private VehicleType vehicleType;
+
+
+    public Vehicle(String vehicleNumber, VehicleType vehicleType) {
+        this.vehicleNumber = vehicleNumber;
+        this.vehicleType = vehicleType;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
 
     public VehicleType getVehicleType() {
         return vehicleType;
