f6fde5544d4a0ae2665ea69a4d08b000e4d1b42f
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-23 06:56:01 +0530

    Concrete Vehicle Classes

---
+++ src/com/vishwas/lld/projects/parkingLot/model/Car.java
@@ -0,0 +1,9 @@
+package com.vishwas.lld.projects.parkingLot.model;
+
+import com.vishwas.lld.projects.parkingLot.enums.VehicleType;
+
+public class Car extends Vehicle{
+    public Car(String vehicleNumber, VehicleType vehicleType) {
+        super(vehicleNumber, vehicleType);
+    }
+}
---
+++ src/com/vishwas/lld/projects/parkingLot/model/Truck.java
@@ -0,0 +1,9 @@
+package com.vishwas.lld.projects.parkingLot.model;
+
+import com.vishwas.lld.projects.parkingLot.enums.VehicleType;
+
+public class Truck extends Vehicle{
+    public Truck(String vehicleNumber, VehicleType vehicleType) {
+        super(vehicleNumber, vehicleType);
+    }
+}
